f61e011d36820f528cb473ae5a33ce72248e766b
Tom O'Brien <thomas.obrien86@gmail.com>
2015-05-17 17:06:30 +0100

    Return mock wordcloud data

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
-# This file provided by Facebook is for non-commercial testing and evaluation purposes only.
-# Facebook reserves all rights not expressly granted.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 import json
 import os
 from flask import Flask, Response, request
@@ -15,17 +5,17 @@ from flask import Flask, Response, request
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
-@app.route('/comments.json', methods=['GET', 'POST'])
-def comments_handler():
+@app.route('/words.json', methods=['GET', 'POST'])
+def words_handler():
 
-    with open('data/comments.json', 'r') as file:
+    with open('data/words.json', 'r') as file:
         comments = json.loads(file.read())
 
-    if request.method == 'POST':
-        comments.append(request.form.to_dict())
+    # if request.method == 'POST':
+    #     comments.append(request.form.to_dict())
 
-        with open('data/comments.json', 'w') as file:
-            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+    #     with open('data/comments.json', 'w') as file:
+    #         file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
 
     return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
 
